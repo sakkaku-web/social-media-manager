719622c5537543fd96f624917d68a189a9f993be
myin <minyin142p@gmail.com>
2022-01-10 17:49:24 +0100

    initial commit

---
+++ apps/social-media-manager/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  displayName: 'social-media-manager',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
+    '^.+\\.[tj]sx?$': 'babel-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/social-media-manager',
+};
