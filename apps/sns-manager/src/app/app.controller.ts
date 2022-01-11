3688d390511b02bd1bde3676e94ba5832233403c
myin <minyin142p@gmail.com>
2022-01-11 08:01:17 +0100

    feat: setup twitter client

--- apps/sns-manager/src/app/app.controller.ts
+++ apps/sns-manager/src/app/app.controller.ts
@@ -1,13 +1,25 @@
 import { Controller, Get } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { TwitterApi } from 'twitter-api-v2';
 
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService,
+    private readonly config: ConfigService) {}
 
   @Get()
-  getData() {
+  async getData() {
+    const userClient = new TwitterApi({
+      appKey: this.config.get('TWITTER_API_KEY'),
+      appSecret: this.config.get('TWITTER_SECRET'),
+      accessToken: this.config.get('TWITTER_CLIENT_ID'),
+      accessSecret: this.config.get('TWITTER_CLIENT_SECRET'),
+    });
+    await userClient.appLogin();
+    const user = await userClient.currentUserV2();
+    console.log(user);
     return this.appService.getData();
   }
 }
