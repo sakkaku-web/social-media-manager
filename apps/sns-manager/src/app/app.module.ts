0b273e05c71d30e57f9ace4796d200f53e9939ab
myin <minyin142p@gmail.com>
2022-01-11 19:55:22 +0100

    feat: setup reddit login

--- apps/sns-manager/src/app/app.module.ts
+++ apps/sns-manager/src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { HttpModule } from '@nestjs/axios';
 
 import { TwitterController } from './twitter/twitter.controller';
+import { RedditController } from './reddit/reddit.controller';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
-  controllers: [TwitterController],
+  imports: [ConfigModule.forRoot(), HttpModule],
+  controllers: [TwitterController, RedditController],
   providers: [],
 })
 export class AppModule {}
---
+++ apps/sns-manager/src/app/reddit/reddit.controller.ts
@@ -0,0 +1,96 @@
+import {
+  Controller,
+  Get,
+  HttpException,
+  Query,
+  Res,
+  Session,
+} from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { HttpService } from '@nestjs/axios';
+import { Response } from 'express';
+import { environment } from '../../environments/environment';
+import { nanoid } from 'nanoid';
+import { map } from 'rxjs';
+
+interface RedditAuthCallback {
+  error?: string;
+  code: string;
+  state: string;
+}
+
+@Controller('reddit')
+export class RedditController {
+  constructor(
+    private readonly config: ConfigService,
+    private readonly httpService: HttpService
+  ) {}
+
+  private get redirectUrl() {
+    return `${environment.baseUrl}/reddit/callback`;
+  }
+
+  private get clientId() {
+    return this.config.get('REDDIT_CLIENT');
+  }
+
+  private get clientSecret() {
+    return this.config.get('REDDIT_SECRET');
+  }
+
+  @Get('login')
+  async login(
+    @Res() res: Response,
+    @Session() session: Record<string, string>
+  ) {
+    const clientId = this.clientId;
+    const callback = this.redirectUrl;
+    const state = nanoid();
+    const url = `https://www.reddit.com/api/v1/authorize?client_id=${clientId}&response_type=code&state=${state}&redirect_uri=${callback}&duration=permanent&scope=identity+submit`;
+
+    session.STATE = state;
+    return res.redirect(url);
+  }
+
+  @Get('callback')
+  async callback(
+    @Query() query: RedditAuthCallback,
+    @Session() session: Record<string, string>
+  ) {
+    const state = session.STATE;
+
+    if (!state) {
+      throw new HttpException(
+        'Missing state. Call /api/reddit/login first',
+        400
+      );
+    }
+
+    if (state != query.state) {
+      throw new HttpException('State mismatch', 400);
+    }
+
+    if (query.error) {
+      throw new HttpException(query.error, 400);
+    }
+
+    const body = `grant_type=authorization_code&code=${query.code}&redirect_uri=${this.redirectUrl}`;
+    const encoded = btoa(`${this.clientId}:${this.clientSecret}`);
+
+    return this.httpService
+      .post('https://www.reddit.com/api/v1/access_token', body, {
+        headers: {
+          Authorization: `Basic ${encoded}`,
+        },
+      })
+      .pipe(
+        map((data) => {
+          if (data.status !== 200) {
+            throw new HttpException(`${data.status} - ${data.data}`, 400);
+          }
+
+          return data.data;
+        })
+      );
+  }
+}
