223fbe6e38ee16f3851d458e5912f79d545255d1
myin <minyin142p@gmail.com>
2022-01-11 20:56:34 +0100

    chore: remove reddit tokens + commit unsaved changes

--- libs/sns/reddit/src/lib/reddit-service.ts
+++ libs/sns/reddit/src/lib/reddit-service.ts
@@ -1,18 +1,10 @@
 import { SocialMediaService, User } from '@kumi-arts/core';
-import { Axios } from 'axios';
 import * as Snoowrap from 'snoowrap';
 
 export class RedditService implements SocialMediaService {
   private client: Snoowrap;
 
   constructor(bearer: string) {
-    // this.client = new Axios({
-    //   baseURL: 'https://oauth.reddit.com/',
-    //   headers: {
-    //     Authorization: `Bearer ${bearer}`,
-    //     'User-Agent': 'kumi-arts:0.0.0 (by /u/illu11)',
-    //   },
-    // });
     this.client = new Snoowrap({
       userAgent: 'kumi-arts:0.0.0 (by /u/illu11)',
       accessToken: bearer,
@@ -20,7 +12,6 @@ export class RedditService implements SocialMediaService {
   }
 
   async getUser(): Promise<User> {
-    // const { data } = await this.client.get('/api/v1/me');
     const user = this.client.getMe();
     const name = await user.name;
 
