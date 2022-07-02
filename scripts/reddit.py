5500c15657e4f1a82f0eb7c14b85e7769b098612
myin <minyin142p@gmail.com>
2022-07-02 18:49:27 +0200

    add text comment after post

--- scripts/reddit.py
+++ scripts/reddit.py
@@ -3,87 +3,123 @@ import requests as req
 from dotenv import load_dotenv
 
 # Script can submit multiple images as single post to multiple subreddits
-#
 # Set token in REDDIT_TOKEN
+#
 # Customize variables below
 
-title = 'Test'
+title = 'Korone + Chimecho'
+text = 'Test Text Comment'
+sendreplies = False
 images = [
-  '/home/sakkaku/Downloads/pixel-ina.png',
-  '/home/sakkaku/Downloads/pixel-gura.png',
-  '/home/sakkaku/Downloads/pixel-ame.png',
+    # Text does not work with single images
+    '/home/sakkaku/Downloads/fuurin.png',
 ]
 
+# TODO: fix flair
 subreddits = [
-  'kumi_yada',
-  'test',
+    # {'sr': 'AnimeART', 'flair': 'Original Content'},
+    {'sr': 'AnimeSketch', 'flair': None},
+    {'sr': 'kumi_yada', 'flair': None},
 ]
 
-### ---------------
+# ---------------
 
 load_dotenv()
 
 baseURL = 'https://oauth.reddit.com'
 token = os.getenv('REDDIT_TOKEN')
 headers = {
-  'Authorization': 'Bearer ' + token,
-  'User-Agent': 'test-script:sns-manager:0.0.1 (by /u/illu11)',
+    'Authorization': 'Bearer ' + token,
+    'User-Agent': 'script:sns-manager:0.0.1 (by /u/illu11)',
 }
 asset_url = 'https://reddit-uploaded-media.s3-accelerate.amazonaws.com'
 
+
 def upload_image(image_path: str):
-  filename = image_path.split('/')[-1]
-  mimetype ='image/{}'.format(filename.split('.')[1])
-  data = {
-    'filepath': filename,
-    'mimetype': mimetype,
-  }
-  res = req.post(baseURL + '/api/media/asset.json', data=data, headers=headers)
-  res.raise_for_status()
-
-  asset_data = res.json()
-  args = asset_data['args']
-  full_url = 'https:{}'.format(args['action'])
-
-  with open(image_path, "rb") as image:
-    upload_data = {item["name"]: item["value"] for item in args["fields"]}
-    res = req.post(full_url, data=upload_data, files={"file": image})
+    filename = image_path.split('/')[-1]
+    mimetype = 'image/{}'.format(filename.split('.')[1])
+    data = {
+        'filepath': filename,
+        'mimetype': mimetype,
+    }
+    res = req.post(baseURL + '/api/media/asset.json',
+                   data=data, headers=headers)
     res.raise_for_status()
 
-  asset_id = asset_data['asset']['asset_id']
-  print('Uploaded {} with id {}'.format(image_path, asset_id))
-  return asset_id
+    asset_data = res.json()
+    args = asset_data['args']
+    full_url = 'https:{}'.format(args['action'])
 
-def post_images_to_subreddit(ids, sr: str, title: str):
-  res = None
-  if len(ids) > 1:
+    with open(image_path, "rb") as image:
+        upload_data = {item["name"]: item["value"] for item in args["fields"]}
+        res = req.post(full_url, data=upload_data, files={"file": image})
+        res.raise_for_status()
+
+    asset_id = asset_data['asset']['asset_id']
+    print('Uploaded {} with id {}'.format(image_path, asset_id))
+    return asset_id
+
+
+def post_images_to_subreddit(ids, sr: dict, title: str):
+    res = None
     body = {
-      'items': [{'media_id': i, 'caption': '', 'outbound_url': ''} for i in ids],
-      'kind': 'self',
-      'sr': sr,
-      'title': title,
+        'sr': sr['sr'],
+        'title': title,
+        'sendreplies': sendreplies,
+        'flair_id': sr['flair'],
     }
-    res = req.post(baseURL + '/api/submit_gallery_post.json?api_type=json', json=body, headers=headers)
-  else:
-    image_id, asset_url = ids[0]
+    if len(ids) > 1:
+        body['kind'] = 'self'
+        body['items']: [{'media_id': i, 'caption': '',
+                         'outbound_url': ''} for i in ids]
+        res = req.post(
+            baseURL + '/api/submit_gallery_post.json?api_type=json', json=body, headers=headers)
+    else:
+        body['kind'] = 'image'
+        body['url'] = f'{asset_url}/rte_images/{ids[0]}',
+        res = req.post(baseURL + '/api/submit?api_type=json',
+                       body, headers=headers)
+
+    data = return_data_if_no_error(res)
+
+    if 'url' in data:
+        print(f'Submitted post in {sr}: {data["url"]}')
+        return data['id']
+
+    print('Submitted single image post. No direct URL available')
+    return None
+
+
+def add_comment(post_id: str, text: str):
     body = {
-      'sr': sr,
-      'title': title,
-      'kind': 'image',
-      'url': f'{asset_url}/rte_images/{image_id}',
+        'text': text,
+        'thing_id': post_id,
     }
-    res = req.post(baseURL + '/api/submit?api_type=json', body, headers=headers)
+    res = req.post(baseURL + '/api/comment?api_type=json',
+                   body, headers=headers)
+    data = return_data_if_no_error(res)
+    print(f'Added comment to post {post_id}: {text}')
 
-  res.raise_for_status()
-  data = res.json()['json']['data']
-  if 'url' in data:
-    return data['url']
-  return data['user_submitted_page']
+    return data['things'][0]['data']['id']
 
+
+def return_data_if_no_error(res):
+    res.raise_for_status()
+
+    json = res.json()['json']
+    errors = json['errors']
+
+    if len(errors) > 0:
+        print(errors)
+        exit()
+
+    return json['data']
 
 
 for sr in subreddits:
-  # Has to be uploaded for each subreddit, otherwise error 500
-  image_ids = [upload_image(i) for i in images]
-  url = post_images_to_subreddit(image_ids, sr, title)
-  print(f'Submitted post in {sr}: {url}')
+    # Has to be uploaded for each subreddit, otherwise error 500
+    image_ids = [upload_image(i) for i in images]
+    post_id = post_images_to_subreddit(image_ids, sr, title)
+
+    if post_id:
+        add_comment(post_id, text)
