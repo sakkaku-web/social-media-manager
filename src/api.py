6cd3f91ae1fefea124ba3516a0a63edd8e065474
myin <minyin142p@gmail.com>
2022-07-06 20:34:00 +0200

    add home

--- src/api.py
+++ src/api.py
@@ -9,3 +9,8 @@ app = Flask(__name__)
 app.secret_key = os.getenv('SESSION_SECRET')
 
 app.register_blueprint(reddit_api, url_prefix='/api/reddit')
+
+
+@app.get('/')
+def index():
+    return 'Welcome to SNS-Manager API. We are working on an OpenAPI UI so you can explore the API!'
