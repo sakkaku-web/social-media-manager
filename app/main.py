1aad7c26611d3a834e34dd9dcb7a0a624fdd95d1
myin <minyin142p@gmail.com>
2022-07-07 20:11:50 +0200

    manually register api

--- app/main.py
+++ app/main.py
@@ -7,6 +7,10 @@ import importlib
 
 from app.auth import security_schemes
 
+import app.api.pixiv as pix
+import app.api.reddit as red
+import app.api.twitter as tw
+
 info = Info(title='SNS-Manager API', version='0.0.1')
 app = OpenAPI(__name__, info=info, security_schemes=security_schemes)
 app.secret_key = os.getenv('SESSION_SECRET')
@@ -28,24 +32,28 @@ def index():
     return 'Hello World'
 
 
-def auto_register_api(bp: APIBlueprint):
-    here = os.path.dirname(__file__)
-    api_dir = os.path.join(here, "api")
-    for root, dirs, files in os.walk(api_dir):
-        for file in files:
-            if file == "__init__.py":
-                continue
-            if not file.endswith(".py"):
-                continue
-            api_file = os.path.join(root, file)
-            rule = re.split(r"app|.py", api_file)[1]
-            api_route = ".".join(rule.split(os.sep)).strip(".")
-            api = importlib.import_module('app.' + api_route)
-            try:
-                bp.register_api(api.api)
-            except AttributeError:
-                print(f"Failed to register api: {api_route}")
+# Doesn't deploy sometimes?
+# def auto_register_api(bp: APIBlueprint):
+#     here = os.path.dirname(__file__)
+#     api_dir = os.path.join(here, "api")
+#     for root, dirs, files in os.walk(api_dir):
+#         for file in files:
+#             if file == "__init__.py":
+#                 continue
+#             if not file.endswith(".py"):
+#                 continue
+#             api_file = os.path.join(root, file)
+#             rule = re.split(r"app|.py", api_file)[1]
+#             api_route = ".".join(rule.split(os.sep)).strip(".")
+#             api = importlib.import_module('app.' + api_route)
+#             try:
+#                 bp.register_api(api.api)
+#             except AttributeError:
+#                 print(f"Failed to register api: {api_route}")
 
+# auto_register_api(api)
 
-auto_register_api(api)
+api.register_api(pix.api)
+api.register_api(red.api)
+api.register_api(tw.api)
 app.register_api(api)
