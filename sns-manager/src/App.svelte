27a065102367866107cc1f8ada106688b7b12599
myin <minyin142p@gmail.com>
2022-07-08 17:45:45 +0200

    setup tailwind

--- sns-manager/src/App.svelte
+++ sns-manager/src/App.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
-  import logo from './assets/svelte.png'
-  import Counter from './lib/Counter.svelte'
+  import logo from "./assets/svelte.png";
+  import Counter from "./lib/Counter.svelte";
+  import "./global.css";
 </script>
 
-<main>
+<main class="bg-black h-full">
   <img src={logo} alt="Svelte Logo" />
   <h1>Hello Typescript!</h1>
 
@@ -22,8 +23,8 @@
 
 <style>
   :root {
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
-      Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+      Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
   }
 
   main {
---
+++ sns-manager/tailwind.config.cjs
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  plugins: [],
+  theme: {
+    extend: {},
+  },
+  content: ["./index.html", './src/**/*.{svelte,js,ts}'], // for unused CSS
+  variants: {
+    extend: {},
+  },
+}
