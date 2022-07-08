115fddf9d46c8a622c03ecbb7daa900ad5839271
myin <minyin142p@gmail.com>
2022-07-08 17:25:16 +0200

    change svelte to typescript

--- sns-manager/src/App.svelte
+++ sns-manager/src/App.svelte
@@ -1,11 +1,11 @@
-<script>
+<script lang="ts">
   import logo from './assets/svelte.png'
   import Counter from './lib/Counter.svelte'
 </script>
 
 <main>
   <img src={logo} alt="Svelte Logo" />
-  <h1>Hello world!</h1>
+  <h1>Hello Typescript!</h1>
 
   <Counter />
 
