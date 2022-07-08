19cc3cdf19967fdb6765e6367befa8ae16b10a91
myin <minyin142p@gmail.com>
2022-07-08 17:49:55 +0200

    add base url

--- sns-manager/vite.config.ts
+++ sns-manager/vite.config.ts
@@ -4,6 +4,7 @@ import postcss from "./postcss.config.cjs";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/social-media-manager/",
   plugins: [svelte()],
   css: {
     postcss,
