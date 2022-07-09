5f7171a773d449d3c6a7263faf243ee7de66b69a
myin <minyin142p@gmail.com>
2022-07-09 09:05:11 +0200

    add openapi generated source

---
+++ sns-manager/src/openapi/apis/index.ts
@@ -0,0 +1,5 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './PixivApi';
+export * from './RedditApi';
+export * from './TwitterApi';
---
+++ sns-manager/src/openapi/models/index.ts
@@ -0,0 +1,10 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './ErrorMessage';
+export * from './Login';
+export * from './OAuthToken';
+export * from './RefreshToken';
+export * from './SNSPostResponse';
+export * from './Token';
+export * from './TwitterToken';
+export * from './UnprocessableEntity';
