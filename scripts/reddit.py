05d3e74dc64e6588e468cb5423a3a73942b25a66
myin <minyin142p@gmail.com>
2022-07-19 21:35:52 +0200

    refresh token + add commands to reddit

--- scripts/reddit.py
+++ scripts/reddit.py
@@ -5,9 +5,12 @@ from dotenv import load_dotenv
 from argparse import ArgumentParser
 from shared import build_images, BASE_URL
 
+REDDIT_API = 'https://oauth.reddit.com'
+
 load_dotenv()
 
 parser = ArgumentParser()
+parser.add_argument('command', type=str)  # submit, delete
 parser.add_argument('--title')
 parser.add_argument('--text')
 parser.add_argument('-i', '--images', nargs='+', default=[])
@@ -18,19 +21,25 @@ parser.add_argument('--refresh', default=False, action='store_true')
 
 args = parser.parse_args()
 
-
+token = os.getenv('REDDIT_TOKEN')
 if args.refresh:
     refresh_token = os.getenv('REDDIT_REFRESH')
-    res = req.post(f'{BASE_URL}/reddit/auth/refresh', json={'refresh_token': refresh_token})
-
+    res = req.post(f'{BASE_URL}/reddit/auth/refresh',
+                   json={'refresh_token': refresh_token})
     print(res.text)
     res.raise_for_status()
 
-else:
-    reddit_header = {'Authorization': f'Bearer {os.getenv("REDDIT_TOKEN")}'}
+    data = res.json()
+    print('Refreshed tokens for reddit. Save the new tokens to your environment file')
 
+    token = data['access_token']
+
+
+reddit_header = {'Authorization': f'Bearer {token}'}
+
+
+def submit():
     files = build_images(args.images)
-
     for sr in args.subreddits:
         split = sr.split(':')
         subreddit = split[0]
@@ -45,3 +54,17 @@ else:
         res = req.post(f'{BASE_URL}/reddit/post', data=data,
                        files=files, headers=reddit_header)
         print(f'{res.status_code}: {res.text}')
+
+
+def delete_all():
+    print('DELETE ALL')
+    # for sr in args.subreddits:
+    #     req.get(f'{REDDIT_API}/r/{sr}')
+
+
+cmd_map = {
+    'submit': submit,
+    'delete': delete_all,
+}
+
+cmd_map.get(args.command)()
